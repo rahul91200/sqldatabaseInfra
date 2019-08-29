7baa6eb9f6f43118740dae4be87d4851f54f46a6
Rahul <rahul9.1200@gmail.com>
2019-08-29 16:11:00 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -27,27 +27,34 @@ resource "azurerm_sql_database" "test" {
 -------------------------------------------------------------------------------------------------
 
 
-data resource "azurerm_resource_group" "test" {
-  name     = "acceptanceTestResourceGroup1"
+#refer to a existing resource group
+data "azurerm_resource_group" "test" {
+  name     = "rahul_RG"
 #  location = "West US"
 }
 
 resource "azurerm_sql_server" "test" {
-  name                         = "mysqlserver"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
+  name                         = "mysqlserverrahull"
+  resource_group_name          = "${data.azurerm_resource_group.test.name}"
   location                     = "West US"
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  tags = {
+	environment = "demooo"
+  }
 }
 
 resource "azurerm_sql_database" "test" {
-  name                = "mysqldatabase"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  name                = "mysqldatabaserahull"
+  resource_group_name = "${data.azurerm_resource_group.test.name}"
   location            = "West US"
   server_name         = "${azurerm_sql_server.test.name}"
+  create_mode         = "Default"
+  edition             = "Basic"
 
   tags = {
-    environment = "production"
+    environment = "demooo"
   }
 }
+
